68e6c3a7f8701c3ec1b46252f1e1245e95e83c88
Oliver Van Rheenen <ovan-rhe@f1r4s18.codam.nl>
2023-12-05 11:53:26 +0100

    norm fix in comments

--- hellshell/libft/src/string/ft_strchr.c
+++ hellshell/libft/src/string/ft_strchr.c
@@ -15,7 +15,7 @@
 /**
  * @brief Locates the first occurrence of c (converted to a char) in the string
  * pointed to by s. The terminating null character is considered to be part of
- * the string; therefore if c is `\0', the functions locate the terminating `\0'.
+ * the string; therefore if c is `\0', the functions locate the terminating `\0'
  * @param s The string to search
  * @param c The character to search for
  * @return A pointer to the located character, or NULL if the character does not
